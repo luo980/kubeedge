dd116ac5d2a3525c7c34217a952c34afd93c9f0e
luo980 <luo980@gmail.com>
2021-11-02 11:47:02 +0800

    pooooooooooooooooooooooooooooooooooooh

--- edge/pkg/dmanager/dmtype/types.go
+++ edge/pkg/dmanager/dmtype/types.go
@@ -17,47 +17,23 @@ type Device struct {
 	Description string              `json:"description,omitempty"`
 	State       string              `json:"state,omitempty"`
 	LastOnline  string              `json:"last_online,omitempty"`
-	Attributes  map[string]*MsgAttr `json:"attributes,omitempty"`
-	Twin        map[string]*MsgTwin `json:"twin,omitempty"`
+	Meta        map[string]*DevMeta `json:"meta,omitempty"`
+	Data        map[string]*DevData `json:"data,omitempty"`
 }
 
-//MsgAttr the struct of device attr
-type MsgAttr struct {
-	Value    string        `json:"value"`
-	Optional *bool         `json:"optional,omitempty"`
-	Metadata *TypeMetadata `json:"metadata,omitempty"`
+//DevMeta the struct of device meta
+type DevMeta struct {
+	Value     string `json:"value,omitempty"`
+	Metadata  string `json:"metadata,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
 }
 
-//TypeMetadata the meta of value type
-type TypeMetadata struct {
-	Type string `json:"type,omitempty"`
-}
-
-//MsgTwin the struct of device twin
-type MsgTwin struct {
-	Expected        *TwinValue    `json:"expected,omitempty"`
-	Actual          *TwinValue    `json:"actual,omitempty"`
-	Optional        *bool         `json:"optional,omitempty"`
-	Metadata        *TypeMetadata `json:"metadata,omitempty"`
-	ExpectedVersion *TwinVersion  `json:"expected_version,omitempty"`
-	ActualVersion   *TwinVersion  `json:"actual_version,omitempty"`
-}
-
-//TwinValue the struct of twin value
-type TwinValue struct {
-	Value    *string        `json:"value,omitempty"`
-	Metadata *ValueMetadata `json:"metadata,omitempty"`
-}
-
-//TwinVersion twin version
-type TwinVersion struct {
-	CloudVersion int64 `json:"cloud"`
-	EdgeVersion  int64 `json:"edge"`
-}
-
-//ValueMetadata the meta of value
-type ValueMetadata struct {
-	Timestamp int64 `json:"timestamp,omitempty"`
+//DevData the struct of device data
+type DevData struct {
+	Value     string `json:"value,omitempty"`
+	Twin      string `json:"twin,omitempty"`
+	Metadata  string `json:"metadata,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
 }
 
 //Parameter container para
@@ -92,12 +68,6 @@ type DealAttrResult struct {
 	Add    []dmdatabase.Device
 	Delete []dmdatabase.DeviceDelete
 	Update []dmdatabase.DeviceAttrUpdate
-	Result map[string]*MsgAttr
+	Result map[string]*DevMeta
 	Err    error
-}
-
-//TwinMsg the struct of twin update
-type TwinMsg struct {
-	BaseMessage
-	Twin map[string]*MsgTwin `json:"twin"`
 }
